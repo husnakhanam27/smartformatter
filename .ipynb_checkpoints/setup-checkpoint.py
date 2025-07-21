3474822bd678969674346f044903dd52d20a2c03
husnakhanam27 <husnakhanambts@gmail.com>
2025-07-21 10:07:10 +0530

    First commit

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+     name='smartormatter',
+     version='0.1',
+     packages=find_packages(),
+     install_requires=['inflect'],
+     author='Husna',
+     description='Utility functions for smart formatting of names, phones, numbers.',
+     )
+
+
+
+
+
